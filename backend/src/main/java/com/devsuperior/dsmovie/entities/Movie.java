25a6b2fc07c626055725fba80cf47eea35a7f7e4
Gersongmax <gersongmax@hotmail.com>
2022-01-16 18:15:16 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -19,6 +23,9 @@ private String title;
 private Double score;
 private Integer count;
 private String image;
+
+@OneToMany(mappedBy ="id.movie")
+private Set<Score> scores = new HashSet<>();
 
 public Movie() {
 	
@@ -72,6 +79,10 @@ public String getImage() {
 
 public void setImage(String image) {
 	this.image = image;
+}
+
+public Set<Score> getScores() {
+	return scores;
 }
 
 
